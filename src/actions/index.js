ec5d0a6b16c315d5fb1b1a1f96626030c30e42d3
Jared <jaredcmurphy@gmail.com>
2016-06-05 01:24:38 -0400

    adds action creator method in search_bar.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,16 @@
+import axios from 'axios';
+
 const API_KEY='45683852bbe910d7d9147f78f770c0b7';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const URL = `${ROOT_URL}&q=${city},us`;
-  
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url)
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
+import { bindActionCreators } from '../actions/index';
 
-
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
@@ -36,3 +37,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
