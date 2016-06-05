f5f94ad4c137c18580c63c6c0303ec4735a68137
Jared <jaredcmurphy@gmail.com>
2016-06-05 01:32:36 -0400

    gets 200 request in console for api request

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect} from 'react-redux';
-import { bindActionCreators } from '../actions/index';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class SearchBar extends Component {
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -18,6 +20,8 @@ class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
