e9e4bf9a942e11fbab8d3df4beb0e8145cb868d6
Jared <jaredcmurphy@gmail.com>
2016-06-05 02:38:58 -0400

    sparkline component renders graphs for temp, pressure, and humidity

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,12 +8,13 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={pressures} color="green" /></td>
+        <td><Chart data={humidities} color="blue" /></td>
       </tr>
     );
   }
@@ -26,7 +27,7 @@ class WeatherList extends Component {
             <th>City</th>
             <th>Temperature</th>
             <th>Pressure</th>
-            <th>Hunidity</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
