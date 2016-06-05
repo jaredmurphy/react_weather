4afe1e1400fb27463ef44809ba92a099cc59c2ed
Jared <jaredcmurphy@gmail.com>
2016-06-05 00:15:47 -0400

    disables form submit

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -14,9 +14,13 @@ export default class SearchBar extends Component {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get A 5 Day Forcast In Your City"
           value={this.state.term}
