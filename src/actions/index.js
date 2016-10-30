c0c421a3059becba15e0fcc9965a24d04c540ab0
Jared <jaredcmurphy@gmail.com>
2016-10-30 12:02:35 -0400

    revert to http

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY='45683852bbe910d7d9147f78f770c0b7';
-const ROOT_URL = `//api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
